4ff6ce6b643f34a7acbeae8155415b5991ffb81a
Matti Tahvonen <matti@vaadin.com>
2024-03-15 11:17:41 +0200

    version update and tiny polishing

--- src/main/java/org/vaadin/marcus/vaadinai/VaadinAiApplication.java
+++ src/main/java/org/vaadin/marcus/vaadinai/VaadinAiApplication.java
@@ -1,20 +1,17 @@
 package org.vaadin.marcus.vaadinai;
 
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.messages.MessageInput;
-import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
-import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.StreamingChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.vaadin.firitin.components.messagelist.MarkdownMessage;
+import org.vaadin.firitin.components.messagelist.MarkdownMessage.Color;
 
-import java.util.Objects;
 import java.util.Optional;
 
 // For native image, compile with ./mvnw -DskipTests -Pnative -Pproduction native:compile
@@ -30,13 +27,13 @@ public class VaadinAiApplication implements AppShellConfigurator {
 			var messageInput = new MessageInput();
 
 			messageInput.addSubmitListener(e -> {
-				var message = e.getValue();
-				var userMessage = new MarkdownMessage(message, "You", 1);
-				var assistantMessage = new MarkdownMessage("Assistant", 2);
+				var question = e.getValue();
+				var userMessage = new MarkdownMessage(question, "You", Color.AVATAR_PRESETS[1]);
+				var assistantMessage = new MarkdownMessage("Assistant", Color.AVATAR_PRESETS[2]);
 
 				messageList.add(userMessage, assistantMessage);
 
-				chatClient.stream(new Prompt(message))
+				chatClient.stream(new Prompt(question))
 						.map(res -> Optional.ofNullable(res.getResult().getOutput().getContent()).orElse(""))
 						.subscribe(assistantMessage::appendMarkdownAsync);
 			});
